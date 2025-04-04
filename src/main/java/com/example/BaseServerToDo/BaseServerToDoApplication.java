cd5d27cfea07c8776d293887059b4fb3befc66f9
Nik-kan <celovaveronika@gmail.com>
2025-04-04 18:49:17 +0300

    first commit

---
+++ src/main/java/com/example/BaseServerToDo/BaseServerToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.BaseServerToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseServerToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseServerToDoApplication.class, args);
+	}
+
+}
